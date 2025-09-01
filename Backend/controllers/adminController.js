7d594320f16b2de9887b37ac292fc7a096854b84
Swaraj <swarajrawate28@gmail.com>
2025-09-01 19:23:24 +0530

    Updated the getuser api sql query

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,8 +1,24 @@
 const db = require('../config/db');
 
 const getAllUsers = async (req , res) => {
+
   try {
-    const [rows] = await db.query('select id , name , email , role from users');
+    const sql = `
+    select u.id AS user_id,
+    u.name ,
+    u.email ,
+    u.role ,
+    e.id AS employee_id ,
+    e.department , 
+    e.designation , 
+    e.manager_id , 
+    e.profile_pic , 
+    e.resume
+    from users u 
+    left join employee e on u.id = e.user_id
+    order by u.id ASC 
+    `;
+    const [rows] = await db.query(sql);
     res.status(200).json(rows);
 
   } catch (error) {
