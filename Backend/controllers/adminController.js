84634ee88dfd9389b05970ec06627c6d1295e543
Swaraj <swarajrawate28@gmail.com>
2025-09-02 08:59:55 +0530

    Impl add createOrUpdateEmployee controller for managing employee records

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,5 +1,6 @@
 const db = require('../config/db');
 
+//this function is for fetching all the users along with their employee data 
 const getAllUsers = async (req , res) => {
 
   try {
@@ -27,4 +28,52 @@ const getAllUsers = async (req , res) => {
   }
 }
 
-module.exports = { getAllUsers };
+//this function is for the admin who will manage all the internal feilds for the employee like assigning the manager selecting the department and designation..
+const createOrUpdateEmployee = async (req , res) => {
+    const {
+      user_id,
+      department,
+      designation,
+      manager_id = null,
+      profile_pic = null,
+      resume = null
+    } = req.body;
+
+    if(!user_id || !department || !designation)
+    {
+      return res.status(400).json({message : "user_id , department , designation feilds required.."});
+    }
+
+    try {
+      const [userRows] = await db.query('select id from users where id = ?' , [user_id]);
+      if(userRows.length === 0)
+      {
+        return res.status(400).json({message:"user_id does not exist."});
+      }
+
+      const[managerRows] = await db.query('select id from users where id = ? AND role IN(? , ?)' , [manager_id , 'admin' , 'manager']);
+      if(managerRows.length === 0)
+      {
+        return res.status(500).json({message : "manager_id does not exist"});
+      }
+
+      const[empRows] = await db.query('select id from employee where user_id = ? ' , [user_id]);
+
+      if(empRows.length > 0)
+      {
+        const updatesql = `UPDATE employee SET department = ? , designation = ? , manager_id = ? , profile_pic = ? , resume = ? WHERE user_id = ?`;
+        await db.query(updatesql , [department , designation , manager_id , profile_pic , resume , user_id]);
+        return res.status(200).json({message : "Employee updated Successfully.."});
+      }else{
+        const insertsql = `insert into employee (user_id , department , designation , manager_id , profile_pic , resume) values(? , ? , ? , ? , ? , ?)`;
+        await db.query(insertsql ,[user_id , department , designation , manager_id , profile_pic , resume]);
+        return res.status(201).json({message : "Employee inserted successfully in employee table"});
+      }
+
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({message : "Database Error"})
+    }
+}
+
+module.exports = { getAllUsers , createOrUpdateEmployee };
--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../controllers/adminController');
+const { getAllUsers , createOrUpdateEmployee} = require('../controllers/adminController');
 
 const{ protect , isAdmin} = require('../middleware/authMiddleware');
 
 router.get('/users' , protect ,isAdmin , getAllUsers);
+router.post('/employee' , protect , isAdmin , createOrUpdateEmployee);
 
 module.exports = router;
